06ea1a25ea43bfce91097451f2262dad1211c060
Tahu <tahuwhite2003@gmail.com>
2023-09-15 13:03:38 +1000

    Finished code in climate_q1.py

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,49 @@
 import matplotlib.pyplot as plt
-        
+
+#use sqlite3 library to connect to database
+import sqlite3
+
+
+
 years = []
 co2 = []
 temp = []
+
+#write some code in climate_q1.py to connect to database,
+#fetch the values, and populate python lists wiht corresponding values.
+
+# connect to db
+con = sqlite3.connect('climate.db')
+
+#make a cursor for executing sql 
+
+cursor = con.cursor()
+
+# Fetch data from the database
+
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData ORDER BY Year")
+
+
+#create tables array containing all data in climate.db
+tables = cursor.fetchall()
+
+#check content of tables
+
+#print(tables)
+
+# iterate through tables, filling the years, co2, and temp arrays
+
+for table in tables:
+    years.append(table[0])
+    co2.append(table[1])
+    temp.append(table[2])
+
+# Close connection to db
+con.close()
+
+
+
+
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
