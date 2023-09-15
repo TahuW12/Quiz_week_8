cc44a68d11a2152eeea334275da6bef09fcb0b16
Tahu <tahuwhite2003@gmail.com>
2023-09-15 13:13:43 +1000

    Finished code for climate_q2.py

--- climate_q2.py
+++ climate_q2.py
@@ -5,6 +5,20 @@ years = []
 co2 = []
 temp = []
 
+# use pandas to open csv and read information
+
+dataframe = pd.read_csv("climate.csv")
+
+# use iteration to add all data to pre made arrays (years, co2, temp)
+
+for data in dataframe["CO2"]:
+    co2.append(data)
+for data in dataframe["Year"]:
+    years.append(data)
+for data in dataframe["Temperature"]:
+    temp.append(data)
+
+
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
